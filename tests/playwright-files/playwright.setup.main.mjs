b52a696a2cbe4f822c0390d59a6abee7417e4e31
Euan Galloway <2388242g@student.gla.ac.uk>
2026-02-11 11:43:34 +0000

    Playwright Deletion

--- tests/playwright-files/playwright.setup.main.mjs
+++
@@ -1,9 +0,0 @@
-import { execSync } from "child_process";
-
-export default async () => {
-  console.log("Compiling main.scss → main.css...");
-  execSync("npx sass src/main.scss dist/main.css --no-source-map", {
-    stdio: "inherit"
-  });
-  console.log("✔ full system compiled");
-};
--- tests/playwright-files/playwright.tokens.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "@playwright/test";
-
-export default defineConfig({
-  testDir: "../tests/token_test",
-  globalSetup: "./playwright.setup.tokens.mjs",
-});
